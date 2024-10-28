23af618fb6c205ba8d09c12858c40ce74df83b0b
Your Name <you@example.com>
2024-10-28 16:23:43 +0900

    wip: change order struct

--- backend/models/order.go
+++ backend/models/order.go
@@ -6,7 +6,7 @@ import (
 
 type Order struct {
 	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	UserID    primitive.ObjectID `json:"userID"`
+	UserID    primitive.ObjectID `bson:"userID" json:"userID"`
 	Products  []OrderProduct     `json:"products"`
 	Total     float64            `json:"totalAmount"`
 	Status    string             `json:"status"`
