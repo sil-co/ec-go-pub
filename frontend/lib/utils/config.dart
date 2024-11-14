91aa2dde4d4f077870cbe081cb2793eb92914bf7
anknown <anknown@example.com>
2024-11-14 15:48:15 +0900

    dev: develop branch apiUrl is localhost

--- frontend/lib/utils/config.dart
+++ frontend/lib/utils/config.dart
@@ -1,6 +1,6 @@
 class Config {
   // 開発環境用API
-  // static const String apiUrl = 'http://localhost:8080';
+  static const String apiUrl = 'http://localhost:8080';
   // 本番環境
-  static const String apiUrl = 'http://133.18.232.127';
+  // static const String apiUrl = 'http://133.18.232.127';
 }
