3ab249a2be093b385751c9378d2431cea40f55f1
anonymous <no@mail.address>
2024-11-15 09:29:11 +0900

    fix: 8080→80

--- frontend/lib/utils/config.dart
+++ frontend/lib/utils/config.dart
@@ -2,5 +2,5 @@ class Config {
   // 開発環境用API
   // static const String apiUrl = 'http://localhost:8080';
   // 本番環境
-  static const String apiUrl = 'http://133.18.232.127:8080/api';
+  static const String apiUrl = 'http://133.18.232.127/api';
 }
